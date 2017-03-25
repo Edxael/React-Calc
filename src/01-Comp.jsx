e1194db4b1a2feb81bbab8cc8e4fa11288c886fe
edxael <zerphyr.r@gmail.com>
2017-03-25 10:20:20 -0600

    Calculator1

---
+++ src/01-Comp.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css';
+
+
+class Comp_01 extends Component{
+    render(){
+        return(
+            <div className="container">
+
+                    <div>Hello from 01-Comp.jsx</div>
+
+
+            </div>
+        )
+    }
+}
+
+export default Comp_01;
